275202d6cb6853ef7306ae103d0506870b047ac4
joallace <wallace.lucena@hotmail.com>
2023-05-24 21:58:14 -0300

    Versão polida

--- P8/GDSCO0068 - Sistemas Operacionais I/Projeto 2 - Substituidor de Páginas/main.cpp
+++ P8/GDSCO0068 - Sistemas Operacionais I/Projeto 2 - Substituidor de Páginas/main.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <vector>
 #include <queue>
-#include <string>
-#include <set>
 
-struct memory_instace
-{
+struct memory_instace{
     std::vector<int> page_references;
     int frames;
 };
 
-memory_instace read_file(char *path)
-{
+memory_instace read_file(char *path){
     std::ifstream file(path);
     memory_instace instance;
     int aux;
@@ -34,30 +29,66 @@ memory_instace read_file(char *path)
     return instance;
 }
 
-void print_instance(std::vector<int> const &page_refereces)
-{
-    printf("[");
-    for (int i = 0; i < page_refereces.size(); i++)
-        printf("%d %s", page_refereces[i], i + 1 == page_refereces.size() ? "]\n" : ", ");
+void FIFO(memory_instace &instance, std::deque<int> &frames, int i, std::vector<int> &entry_order){
+    frames.pop_front();
+    frames.push_back(instance.page_references[i]);
 }
 
-void print_instance(std::deque<int> const &page_refereces)
-{
-    printf("[");
-    for (int i = 0; i < page_refereces.size(); i++)
-        printf("%d %s", page_refereces[i], i + 1 == page_refereces.size() ? "]\n" : ", ");
+void optimal(memory_instace &instance, std::deque<int> &frames, int i, std::vector<int> &entry_order){
+    bool inside;
+
+    entry_order.clear();
+    for(int j=i; (entry_order.size()<frames.size()) || (j<instance.page_references.size()); j++){
+        inside=false;
+        for(int k=0; k<entry_order.size(); k++)
+            if(instance.page_references[j] == entry_order[k]){
+                    inside=true;
+                    break;
+                }
+        if(!inside)
+            entry_order.push_back(instance.page_references[j]);
+    }
+
+    for(int j=0; j<frames.size(); j++)
+        if(frames[j] == entry_order.back()){
+            frames[j] = instance.page_references[i];
+            return;
+        }
 }
 
-int FIFO(memory_instace instance){
-    std::deque<int> frames;
+
+void LRU(memory_instace &instance, std::deque<int> &frames, int i, std::vector<int> &entry_order){
+    bool inside;
+
+    entry_order.clear();
+    for(int j=i; (entry_order.size()<frames.size()) || (j>=0); j--){
+        inside=false;
+        for(int k=0; k<entry_order.size(); k++)
+            if(instance.page_references[j] == entry_order[k]){
+                inside=true;
+                break;
+            }
+        if(!inside)
+            entry_order.push_back(instance.page_references[j]);
+    }
+
+    for(int j=0; j<frames.size(); j++)
+        if(frames[j] == entry_order.back()){
+            frames[j] = instance.page_references[i];
+            return;
+        }
+}
+
+int getNumberOfFaults(memory_instace& instance, void (*algorithm)(memory_instace&, std::deque<int>&, int, std::vector<int>&)){
+    std::deque<int> frames(instance.frames);
+    std::vector<int> entry_order;
     int faults = instance.frames;
     bool inside;
 
-    for(int i=0; i<instance.frames; i++)
-        frames.push_back(instance.page_references[i]);
+    for(int i=0; i<frames.size(); i++)
+        frames[i] = instance.page_references[i];
 
     for(int i=instance.frames; i<instance.page_references.size(); i++){
-        print_instance(frames);
         inside = false;
         for(int j=0; j<instance.frames; j++)
             if(instance.page_references[i] == frames[j]){
@@ -67,114 +98,20 @@ int FIFO(memory_instace instance){
 
         if(!inside){
             faults++;
-            frames.pop_front();
-            frames.push_back(instance.page_references[i]);
+            algorithm(instance, frames, i, entry_order);
         }
     }
 
     return faults;
 }
 
-int optimal(memory_instace instance){
-    std::deque<int> frames(instance.frames);
-    std::vector<int> entry_order;
-    int faults = instance.frames;
-    bool inside;
 
-    for(int i=0; i<frames.size(); i++)
-        frames[i] = instance.page_references[i];
+int main(int argc, char *argv[]){
+    memory_instace i = read_file(argv[1]);
 
-    for(int i=frames.size(); i<instance.page_references.size(); i++){
-        inside = false;
-        for(int j=0; j<frames.size(); j++)
-            if(instance.page_references[i] == frames[j]){
-                inside = true;
-                break;
-            }
+    printf("FIFO: %d\n",  getNumberOfFaults(i, &FIFO));
+    printf("OTM: %d\n",  getNumberOfFaults(i, &optimal));
+    printf("LRU: %d\n", getNumberOfFaults(i, &LRU));
 
-        if(!inside){
-            faults++;
-
-            entry_order.clear();
-            for(int j=i; (entry_order.size()<frames.size()) || (j<instance.page_references.size()); j++){
-                inside=false;
-                for(int k=0; k<entry_order.size(); k++)
-                    if(instance.page_references[j] == entry_order[k]){
-                            inside=true;
-                            break;
-                        }
-                if(!inside)
-                    entry_order.push_back(instance.page_references[j]);
-            }
-            for(int j=0; j<frames.size(); j++)
-                if(frames[j] == *entry_order.rbegin())
-                    frames[j] = instance.page_references[i];
-        }
-        print_instance(frames);
-
-    }
-
-    return faults;
-}
-
-int LRU(memory_instace instance){
-    std::deque<int> frames(instance.frames);
-    std::vector<int> entry_order;
-    int faults = instance.frames;
-    bool inside;
-    print_instance(instance.page_references);
-
-    for(int i=0; i<frames.size(); i++)
-        frames[i] = instance.page_references[i];
-
-    for(int i=frames.size(); i<instance.page_references.size(); i++){
-        inside = false;
-        for(int j=0; j<frames.size(); j++)
-            if(instance.page_references[i] == frames[j]){
-                inside = true;
-                break;
-            }
-
-        if(!inside){
-            faults++;
-
-            entry_order.clear();
-            for(int j=i; (entry_order.size()<frames.size()) || (j>=0); j--){
-                // printf("%d\n", instance.page_references[j]);
-                inside=false;
-                for(int k=0; k<entry_order.size(); k++)
-                    if(instance.page_references[j] == entry_order[k]){
-                        inside=true;
-                        break;
-                    }
-                if(!inside)
-                    entry_order.push_back(instance.page_references[j]);
-            }
-
-            for (auto it = entry_order.begin(); it != entry_order.end(); ++it)
-                std::cout << ' ' << *it;
-            for(int j=0; j<frames.size(); j++)
-                if(frames[j] == *entry_order.rbegin()){
-                    printf("UEPA\n");
-                    frames[j] = instance.page_references[i];
-                }
-        }
-        print_instance(frames);
-
-    }
-
-    return faults;
-}
-
-
-int main()
-{
-    memory_instace i = read_file("inputs/especificação.txt");
-
-    printf("Frames: %d\n", i.frames);
-    print_instance(i.page_references);
-    printf("\nFIFO: %d\n", FIFO(i));
-    printf("OTM: %d\n", optimal(i));
-    printf("LRU: %d\n", LRU(i));
     return 0;
 }
